e05b525a25c65bcb881bed68fe838fcf37ead879
nico <nico.cristian@itbss.ac.id>
2024-05-01 16:50:57 +0700

    save changes nico

--- php/config.php
+++ php/config.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect("127.0.0.1", "root", "", "verdecreative-oop") or die("Couldn't connect: " . mysqli_connect_error());
+$conn = mysqli_connect("127.0.0.1", "root", "", "verdecreative_oop") or die("Couldn't connect: " . mysqli_connect_error());
 
 ?>
